60a61c3be25a874ba4a5546864005621703b4c9d
George Shao <GeorgeShao123@gmail.com>
2022-01-08 02:56:42 -0500

    🔧 Setup AWS Amplify and CreateAccountPage route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 import LandingPage from "./pages/LandingPage"
+import CreateAccountPage from "./pages/CreateAccountPage"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <LandingPage />
+        </Route>
+        <Route exact path="/signup">
+          <CreateAccountPage />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/pages/CreateAccountPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateAccountPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CreateAccountPage
