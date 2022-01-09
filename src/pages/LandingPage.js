8dd33a9cf5c295b34400b4db2d52a56345d91544
George Shao <GeorgeShao123@gmail.com>
2022-01-09 01:54:10 -0500

    🏞 Updated landing page to use static image

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Hero from "../components/sections/Hero";
 import LandingLayout from "../components/layouts/LandingLayout";
+import leaves from "../assets/leaves.jpg"
 
 export default function LandingPage() {
   return (
@@ -9,7 +10,7 @@ export default function LandingPage() {
       <Hero
         title="Resume management simplified."
         subtitle="Powerful version control and sharing features at your fingertips."
-        image="https://source.unsplash.com/collection/404339/800x600"
+        image={leaves}
         ctaText="Get started for free now!"
         ctaLink="/dashboard"
       />
